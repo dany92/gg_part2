b3c8f014b4bee1fb54921d71ac4962a267cface5
dany92 <dany@bc.edu>
2016-05-16 22:06:47 -0500

    sumbit guess and alert the result.

--- guessingGame.js
+++ guessingGame.js
@@ -11,25 +11,32 @@ var playersGuess,
 // Generate the Winning Number
 
 function generateWinningNumber(){
-	// add code here
+	return Math.floor((Math.random()*100)+1);
 }
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
-	// add code here
+	return +$('input').val();
 }
 
 // Determine if the next guess should be a lower or higher number
 
-function lowerOrHigher(){
-	// add code here
+function lowerOrHigher(winNum, guess){
+	//Display the result on the DOM. For now, alert
+	var higher = (winNum > guess) ? true:false;
+	return((higher)? "winning number is higher":"winning number is lower");
 }
 
 // Check if the Player's Guess is the winning number 
 
-function checkGuess(){
-	// add code here
+function checkGuess(winNum, guess){
+	return (winNum === guess)? true:false;
+}
+
+//return a string that will be used in the DOM message when the checkGuess function is invoked.
+function guessMessage(){
+
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
@@ -44,9 +51,48 @@ function playAgain(){
 	// add code here
 }
 
+//constructor
+var Player = function(){
+	this.numGuess = 0;
+	this.guesses = [];
+}
+
+Player.prototype.filterGuess = function(guess){
+	console.log(this.guesses);
+	if(this.guesses.indexOf(guess)> -1){
+		return false;
+	}
+	this.numGuess++;
+	this.guesses.push(guess);
+	return true;
+}
+
 $(document).ready(function(){
-	$('h1').text("Changed Heading");
-	alert("working!");
-})
+	var player = new Player();
+	var winningNumber = generateWinningNumber();
+	console.log("winning number "+winningNumber);
+
+	$('#guess').on('click', function(){
+		var guess = playersGuessSubmission();
+		console.log("your guess "+guess);
+		//check guess
+		if(player.filterGuess(guess)){
+			var result = checkGuess(winningNumber, guess);
+			if(result){
+				//guessesd the right number!
+				alert("congrats! you won");
+			}else{
+				lowerOrHigher(winningNumber, guess);
+			}
+		}else{
+			//duplicate guess
+			alert("duplicate guess!");
+		}
+	});
+
+
+
+
+});
 
 /* **** Event Listeners/Handlers ****  */
